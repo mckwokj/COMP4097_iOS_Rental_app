192d12c79cff9d5e7faee077d2d7c36940ca580b
Johnson Kwok <mckwokj@gmail.com>
2020-11-03 11:08:51 +0800

    Initial Commit

---
+++ EstateRental/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EstateRental
+//
+//  Created by Man Chun Kwok on 3/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
